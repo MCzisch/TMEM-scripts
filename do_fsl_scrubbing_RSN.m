80599359793179fea426e06fd4614ff14f13da0e
MCzisch <czisch@psych.mpg.de>
2016-10-07 15:21:04 +0200

    Update do_fsl_scrubbing_RSN.m

--- do_fsl_scrubbing_RSN.m
+++ do_fsl_scrubbing_RSN.m
@@ -12,19 +12,21 @@ fprintf(fid,'\n\n\n%s',tmp);
 
 basis_fmri = '/media/spectro/_media_spectro/RAID/data/TMEM_RSN/';
 cd(basis_fmri);
+
+% data input is run via a file list
 fid = fopen('file_list.txt','r');
 tline = fgetl(fid);
 
 while ischar(tline)
 
-    dir = tline(1:7);
+    dir = tline(1:7);   % here, only the first part of the file_list entries is relevant, e.g. RSN_002
     cd (dir);
     
     fprintf(fid, 'running %s\n', dir)
     
     out_cmd = 'rm raw4D';
     system(out_cmd);
-    out_cmd = 'fslmerge -t raw4D vol*nii';
+    out_cmd = 'fslmerge -t raw4D vol*nii';  % scrubbing is done on the raw images
     system(out_cmd);
     out_cmd = 'fsl_motion_outliers -i raw4D.nii -o fsl_motion_outliers.txt -s metric_val.txt -p metric_graph --dummy=5 -v';
     system(out_cmd);
